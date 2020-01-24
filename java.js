cb470492c4979baf0e080228f5ddcdf0bfbec3ec
jTijerino13 <55290206+jTijerino13@users.noreply.github.com>
2020-01-24 11:57:59 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,18 @@
+function myLoop(){
+var end = document.getElementById("end").value; 
+var start = document.getElementById("start").value;
+var jump = document.getElementById("jump").value;
+  
+  console.log(typeof start);
+  console.log(typeof end);
+  console.log(typeof jump);
+  
+start = Number(start);
+end = Number(end);
+jump = Number(jump);
+  
+  for(var i=start;i<=end;i+=jump){
+    document.write('<p>'+i);
+  }
 
+}
